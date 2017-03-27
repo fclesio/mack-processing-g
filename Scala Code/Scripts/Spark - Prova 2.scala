1f4a90d3ffb781da6890a1aafb5236bd12b62835
Flavio Clesio <flavioclesio@yahoo.com>
2017-03-27 18:47:25 -0300

    Exercicios Aula
    Code = Fun

---
+++ Scala Code/Scripts/Spark - Prova 2.scala
@@ -0,0 +1,55 @@
+
+
+// 1) Realize a carga do script do filme que foi indicado para voce (INCEPTION) em um RDD chamado scriptRDD_NOMEFILME
+
+// 2) Realize a contagem do numero de objetos dentro do Dataframe
+
+// 3) Faca a contagem de palavras relativa ao seu respectivo filme
+
+// 4) Faca a carga do arquivo housing.txt que esta no github
+
+// 5) Carregue um objeto que vai buscar os numeros do inFile realizando a quebra por "," (virgula)
+
+// 6) Faca a busca os 7 primeiros elementos dentro do RDD
+
+// 7) Usando a a propriedade flatMap separe cada elemento do arquivo data e insira em um RDD
+
+// 8) Use a propriedade collect para exibir o que tem dentro do RDD
+
+// 9)  Realize a soma dos objetos do RDD
+
+// 10) Realize a contagem dos algarismos dentro do seu RDD 
+
+// 11) Faca a carga do arquivo Brasileiro 2015 dentro de um RDD
+
+// 12) Realize a gravacao do arquivo no seu desktop em uma pasta chamada classificacao
+
+// 13) Realize o import das libs io.Text e a io.IntWritable
+
+// 14) Realize a carga do objeto salvo no seu desktop
+
+// 15) Realize a exibicao dos dados desse novo objeto usando a propriedade collect()
+
+// 16) Abra uma nova sessao no Spark
+
+// 17) Insira a propriedade que faz conversao implicita  
+
+// 18) Crie um dataframe com o arquivo brasileiro_2015.json
+
+// 19) Realize a operacao de exibir o schema, e indique quais sao os datatypes
+
+// 20) Selecione apenas a coluna time
+
+// 21) Divida os pontos dos times por 3 para estimar o numero medio de vitorias desconsideranco outros fatores
+
+// 22) Filtre todos os times com mais de 100 cartoes amarelos
+
+// 23) Divida o numero de gols pro (GP) pelas 38 rodadas e indique o numero medio de gol por time 
+
+// 24) Crie uma view temporaria com o conjunto de dados acima com o nome times em seguida insira em um objeto e exiba usando a opcao show()
+
+// 25) Faca o instanciamento de view global chamada time
+
+// 26) Realize a consulta na view atrelado em uma base de dados no banco de dados 'global.temp'
+
+// 27) Abrindo uma nova sessao realize a consulta na view global
